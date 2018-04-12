fa6ee48f15c90211b6f35352edb7ea5ceb4a1a3e
ANonymous <postmaster.richmama@gmail.com>
2018-04-12 13:53:53 +0100

    added more assets

--- resources/views/foot.blade.php
+++ resources/views/foot.blade.php
@@ -1,36 +1,36 @@
 		<!-- all js here -->
 		<!-- jquery js -->
-		<script src="{{asset('js/vendor/jquery-1.12.0.min.css')}}"></script>	
+		<script src="{{asset('js/vendor/jquery-1.12.0.min.js')}}"></script>	
 		<!-- bootstrap js -->
-		<script src="{{asset('js/bootstrap.min.css')}}"></script>
+		<script src="{{asset('js/bootstrap.min.js')}}"></script>
 		<!-- nivo slider -->
-		<script src="{{asset('js/jquery.nivo.slider.pack.css')}}"></script>
+		<script src="{{asset('js/jquery.nivo.slider.pack.js')}}"></script>
 		<!-- owl carousel js --> 
-        <script src="{{asset('js/owl.carousel.min.css')}}"></script>
+        <script src="{{asset('js/owl.carousel.min.js')}}"></script>
         <!-- mixitup js -->
-        <script src="{{asset('js/jquery.mixitup.css')}}"></script>
+        <script src="{{asset('js/jquery.mixitup.js')}}"></script>
 		<!-- wow js -->
-		<script src="{{asset('js/wow.min.css')}}"></script>
+		<script src="{{asset('js/wow.min.js')}}"></script>
 		<!-- jquery.counterup js -->
-        <script src="{{asset('js/jquery.counterup.min.css')}}"></script>
-		<script src="{{asset('js/waypoints-min.css')}}"></script>
+        <script src="{{asset('js/jquery.counterup.min.js')}}"></script>
+		<script src="{{asset('js/waypoints-min.js')}}"></script>
 		<!-- jquery-ui js -->
-        <script src="{{asset('js/jquery-ui.min.css')}}"></script>
+        <script src="{{asset('js/jquery-ui.min.js')}}"></script>
 		<!-- jquery.countdown js -->
-		<script src="{{asset('js/jquery.countdown.min.css')}}"></script>
+		<script src="{{asset('js/jquery.countdown.min.js')}}"></script>
 		<!-- meanmenu js -->
-        <script src="{{asset('js/jquery.meanmenu.css')}}"></script>
+        <script src="{{asset('js/jquery.meanmenu.js')}}"></script>
         <!-- meanmenu js -->
-        <script src="{{asset('js/ajax-mail.css')}}"></script>
+        <script src="{{asset('js/ajax-mail.js')}}"></script>
 		<!-- elevateZoom js -->
-        <script src="{{asset('js/jquery.elevateZoom-3.0.8.min.css')}}"></script>    
+        <script src="{{asset('js/jquery.elevateZoom-3.0.8.min.js')}}"></script>    
         <!-- Magnific Popup core JS file -->
-		<script src="{{asset('js/jquery.fancybox.pack.css')}}"></script>
+		<script src="{{asset('js/jquery.fancybox.pack.js')}}"></script>
 		<!-- scrollUp js -->
-        <script src="{{asset('js/jquery.scrollUp.css')}}"></script>
+        <script src="{{asset('js/jquery.scrollUp.js')}}"></script>
 		<!-- plugins js -->
-		<script src="{{asset('js/plugins.css')}}"></script>
+		<script src="{{asset('js/plugins.js')}}"></script>
 		<!-- main js -->
-		<script src="{{asset('js/main.css')}}"></script>
+		<script src="{{asset('js/main.js')}}"></script>
 	</body>
 </html>
