cd26a287a6f89e46f1e463ba550bc2afabac7651
ANonymous <postmaster.richmama@gmail.com>
2018-04-12 14:10:01 +0100

    added cart images

--- resources/views/cart-content.blade.php
+++ resources/views/cart-content.blade.php
@@ -18,7 +18,7 @@
 									</thead>
 									<tbody>
 										<tr>
-											<td class="product-thumbnail"><a href="#"><img src="{{asset('img/cart/cart.jpg')}}" alt="" /></a></td>
+											<td class="product-thumbnail"><a href="#"><img src="{{asset('img/cart/1.jpg')}}" alt="" /></a></td>
 											<td class="product-name"><a href="#">Vestibulum suscipit</a></td>
 											<td class="product-price"><span class="amount">$165.00</span></td>
 											<td class="product-quantity"><input type="number" value="1" /></td>
@@ -26,7 +26,7 @@
 											<td class="product-remove"><a href="#"><i class="fa fa-times"></i></a></td>
 										</tr>
 										<tr>
-											<td class="product-thumbnail"><a href="#"><img src="{{asset('img/cart/cart2.jpg')}}" alt="" /></a></td>
+											<td class="product-thumbnail"><a href="#"><img src="{{asset('img/cart/2.jpg')}}" alt="" /></a></td>
 											<td class="product-name"><a href="#">Vestibulum dictum magna</a></td>
 											<td class="product-price"><span class="amount">$50.00</span></td>
 											<td class="product-quantity"><input type="number" value="1" /></td>
