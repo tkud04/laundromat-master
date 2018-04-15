8bfd2087dab7bf7250bce6d8f3b9c5d2835f591f
ANonymous <postmaster.richmama@gmail.com>
2018-04-15 13:56:45 +0100

    adding finishing touches

--- resources/views/product-area.blade.php
+++ resources/views/product-area.blade.php
@@ -6,26 +6,30 @@
 						<div class="product-list">
 							<h3>Best Selling</h3>
 							@if(isset($bs) && count($bs) > 0)
+							@foreach($bs as $b)
+						      <?php
+								$url = "products/".$b['id'];
+								$images = $b['images'];
+							  ?>
 							<div class="single-product-list">
 								<div class="product-image">
-									<a href="product-details.html">
-										<img class="primary-1" src="{{asset('img/product/3.jpg')}}" alt="">
-										<img class="primary-2" src="{{asset('img/product/4.jpg')}}" alt="">
+									<a href="{{$url}}">
+										<img class="primary-1" src="{{$images[0]}}" alt="">
+										<img class="primary-2" src="{{$images[1]}}" alt="">
 									</a>
 								</div>
 								<div class="product-desc">
-									<h4 class="name"><a href="#">Ullamco laboris nisi</a></h4>
+									<h4 class="name"><a href="#">{{$p['name']}}</a></h4>
 									<span class="amount">
-										<del><span class="amount-del">$170.00</span></del>
-										$185.00
+										<del><span class="amount-del">${{$p['price'] + 200}}</span></del>
+										${{$p['price']}}
 									</span>
 									<div class="add-to-cart">
 										<a href="#" data-toggle="tooltip"  title="Add to cart"><i class="fa fa-shopping-cart"></i></a>
-										<a href="#" data-toggle="tooltip"  title="Wishlist" ><i class="fa fa-heart"></i></a>
-										<a href="#" data-toggle="tooltip" data-placement="top" title="Compare" ><i class="fa fa-retweet"></i></a>
 									</div>
 								</div>
 							</div>
+							@endforeach
 							@endif
 							<div class="pro-more-option">
 								<a href="{{url('shop')}}">View more <i class="vc_btn3-icon fa fa-long-arrow-right"></i></a>
@@ -35,56 +39,68 @@
 					<div class="col-md-4 col-sm-6 col-xs-12">
 						<div class="product-list">
 							<h3>On Sale</h3>
+							@if(isset($os) && count($os) > 0)
+							@foreach($os as $o)
+						      <?php
+								$url = "products/".$o['id'];
+								$images = $o['images'];
+							  ?>							
 							<div class="single-product-list">
 								<div class="product-image">
-									<a href="product-details.html">
-										<img class="primary-1" src="{{asset('img/product/7.jpg')}}" alt="">
-										<img class="primary-2" src="{{asset('img/product/1.jpg')}}" alt="">
+									<a href="{{$url}}">
+										<img class="primary-1" src="{{$images[0]}}" alt="">
+										<img class="primary-2" src="{{$images[1]}}" alt="">
 									</a>
 								</div>
 								<div class="product-desc">
-									<h4 class="name"><a href="#">Ullamco laboris nisi</a></h4>
+									<h4 class="name"><a href="#">{{$o['name']}}</a></h4>
 									<span class="amount">
-										<del><span class="amount-del">$170.00</span></del>
-										$185.00
+										<del><span class="amount-del">${{$o['price'] + 200}}</span></del>
+										${{$o['price']}}
 									</span>
 									<div class="add-to-cart">
 										<a href="#" data-toggle="tooltip"  title="Add to cart"><i class="fa fa-shopping-cart"></i></a>
-										<a href="#" data-toggle="tooltip"  title="Wishlist" ><i class="fa fa-heart"></i></a>
-										<a href="#" data-toggle="tooltip" data-placement="top" title="Compare" ><i class="fa fa-retweet"></i></a>
 									</div>
 								</div>
 							</div>
+							@endforeach
+							@endif
 							<div class="pro-more-option">
-								<a href="#">View more <i class="vc_btn3-icon fa fa-long-arrow-right"></i></a>
+								<a href="{{url('shop')}}">View more <i class="vc_btn3-icon fa fa-long-arrow-right"></i></a>
 							</div>
 						</div>
 					</div>
 					<div class="col-md-4 hidden-sm col-xs-12">
 						<div class="product-list last">
 							<h3>Special</h3>
+							@if(isset($ss) && count($ss) > 0)
+							@foreach($ss as $s)
+						      <?php
+								$url = "products/".$s['id'];
+								$images = $s['images'];
+							  ?>
 							<div class="single-product-list">
 								<div class="product-image">
-									<a href="product-details.html">
-										<img class="primary-1" src="{{asset('img/product/2.jpg')}}" alt="">
-										<img class="primary-2" src="{{asset('img/product/1.jpg')}}" alt="">
+									<a href="{{$url}}">
+										<img class="primary-1" src="{{$images[0]}}" alt="">
+										<img class="primary-2" src="{{$images[1]}}" alt="">
 									</a>
 								</div>
 								<div class="product-desc">
-									<h4 class="name"><a href="#">Ullamco laboris nisi</a></h4>
+									<h4 class="name"><a href="#">{{$s['name']}}</a></h4>
 									<span class="amount">
-										<del><span class="amount-del">$170.00</span></del>
-										$185.00
+										<del><span class="amount-del">${{$s['price'] + 200}}</span></del>
+										${{$s['price']}}
 									</span>
 									<div class="add-to-cart">
 										<a href="#" data-toggle="tooltip"  title="Add to cart"><i class="fa fa-shopping-cart"></i></a>
-										<a href="#" data-toggle="tooltip"  title="Wishlist" ><i class="fa fa-heart"></i></a>
-										<a href="#" data-toggle="tooltip" data-placement="top" title="Compare" ><i class="fa fa-retweet"></i></a>
 									</div>
 								</div>
 							</div>
+							@endforeach
+							@endif
 							<div class="pro-more-option">
-								<a href="#">View more <i class="vc_btn3-icon fa fa-long-arrow-right"></i></a>
+								<a href="{{url('shop')}}">View more <i class="vc_btn3-icon fa fa-long-arrow-right"></i></a>
 							</div>
 						</div>
 					</div>
