bdbdebcb4fd986ba9f4371a0402c342604fe6f73
ANonymous <postmaster.richmama@gmail.com>
2018-04-15 13:03:16 +0100

    adding finishing touches

--- resources/views/slider.blade.php
+++ resources/views/slider.blade.php
@@ -7,19 +7,16 @@
 						<div class="search-area">
 							<div class="col-md-12 col-sm-12 col-xs-12">
 								<div class="search-catagory">
-									<form>
-										<div class="select-style">
-											<select class="select-optn ">
-												<option class="first-option">all catagory </option>
-												<option> catagory 1</option>
-												<option> catagory 2</option>
-												<option> catagory 3</option>
-												<option> catagory 4 ff</option>
-											</select>
-										</div>
-										<input class="input-text-box" spellcheck="false" value="" placeholder="Search product...">
-										<button class="btn search-button"><i class="fa fa-search"></i></button>
-									</form>
+							<form action="{{url('search')}}" method="post">
+								{!! csrf_field() !!}
+								<div class="select-style">
+									<select class="select-optn ">
+										<option class="first-option">all category</option>
+									</select>
+								</div>
+								<input class="input-text-box" name="term" spellcheck="false" value="" placeholder="Search product...">
+								<button class="btn search-button"><i class="fa fa-search"></i></button>
+							</form>
 								</div>
 							</div>
 						</div>
